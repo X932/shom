735177c2ad77bf4c6897db8a48353cef57efc751
x932 <yusufgadoiboev@gmail.com>
2024-03-20 08:02:18 +0500

    +

--- backend/src/modules/media/media.controller.ts
+++ backend/src/modules/media/media.controller.ts
@@ -4,13 +4,16 @@ import {
   Delete,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ROUTES } from '@constants/routes';
+import { JwtAuthGuard } from '@guards/jwt.guard';
 import { MediaService } from './media.service';
 import { DeleteMediaDto } from './models/media.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller(ROUTES.MEDIA)
 export class MediaController {
   constructor(private readonly mediaService: MediaService) {}
